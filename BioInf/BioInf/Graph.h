a8a906b195932230dd13eea2cd2f93c5cb35be1a
mgospod <mirela.gospodinovic@fer.hr>
2017-01-14 23:55:27 +0100

    Add function Trim()

--- BioInf/BioInf/Graph.h
+++ BioInf/BioInf/Graph.h
@@ -20,6 +20,14 @@ public:
 		overlap = _overlap;
 		sourceNode = _sourceNode;
 	}
+	unsigned int opposite(unsigned int vertexID) {
+		if (overlap->aID() == vertexID) {
+			return overlap->bID();
+		}
+		else if (overlap->bID() == vertexID) {
+			return overlap->aID();
+		}
+	}
 };
 
 class Vertex {
@@ -39,6 +47,11 @@ public:
 			edges_e.push_back(_edge);//bridovi koji koriste readov kraj
 		else
 			edges_b.push_back(_edge);
+	}
+	bool isTipCandidate() {
+		if (edges_b.size() == 0 || edges_e.size() == 0)
+			return true;
+		return false;
 	}
 };
 
@@ -71,29 +84,71 @@ public:
 		}
 	}
 
-	bool removeNodesWithNoEdges() {//ako je došlo do promjena vrati true
-		vector<unsigned int> verticesToRemove;
-		bool changes = false;//obrisali smo barem jedan čvor
+	Vertex getVertexById(unsigned int vertexId) {
+		map<unsigned int, Vertex>::iterator it;
+		for (it = vertices.begin(); it != vertices.end(); it++) {
+			if (it->first == vertexId)
+				return it->second;
+		}
+	}
+
+	bool trim() {//ako je došlo do promjena vrati true
+		vector<unsigned int> markedVertices;//razlikujemo ih po readID
+		bool changes = false;//nismo još obrisali nijedan čvor
 
 		map<unsigned int, Vertex>::iterator it;
 		for (it = vertices.begin(); it != vertices.end(); it++) {
-			//it->first=IdRead
-			//it->second=vertex
-			if ((it->second).edges_b.size() == 0 && (it->second).edges_e.size() == 0)
-				verticesToRemove.push_back(it->first);
+			//it->first == IdRead, it->second == vertex
+			int sizeEdges_b = (it->second).edges_b.size();
+			int sizeEdges_e = (it->second).edges_e.size();
+			//removingNodesWithNoEdges
+			if ( sizeEdges_b==0 && sizeEdges_e==0 ) { 
+				markedVertices.push_back(it->first);
+				continue;
+			}
+			//removingTrims
+			if (sizeEdges_b == 0 || sizeEdges_e == 0) {
+				vector<Edge> _edges;
+				if (sizeEdges_b == 0 && sizeEdges_e != 0) {
+					_edges = (it->second).edges_e;
+				}
+				if (sizeEdges_b != 0 && sizeEdges_e == 0) {
+					_edges = (it->second).edges_b;
+				}
+				for (int i = 0; i < _edges.size(); i++) {
+					//check if the opposing vertex has a similar edge
+					unsigned int overtexId = _edges[i].opposite(it->first);//opposite vertex ID
+					Vertex overtex = this->getVertexById(overtexId);
+					vector<Edge> oedges = _edges[i].overlap->part(overtexId) == true ? overtex.edges_b : overtex.edges_e;
+					bool isTip = false;
+					for (int j = 0; j < oedges.size(); j++) {
+						//vertex is a tip only if the vertex on the similar edge is not
+						unsigned int oppID = oedges[j].opposite(overtexId);
+						Vertex oppVertex = this->getVertexById(oppID);
+						if (!oppVertex.isTipCandidate()) {
+							isTip = true;
+							break;
+						}
+					}
+					if (isTip) {
+						markedVertices.push_back(it->first);
+					}
+				}
+			}
+				
+
 		}
-		if (verticesToRemove.size() != 0) {
+
+		if (markedVertices.size() != 0) {
 			changes = true;
-			for (int i = 0; i < verticesToRemove.size(); i++) {
-				vertices.erase(verticesToRemove[i]);
+			for (int i = 0; i < markedVertices.size(); i++) {
+				vertices.erase(markedVertices[i]);
 			}
 		}
 		return changes;
+	
 	}
 
-	bool trim() {
-		//ako je došlo do promjena vrati true
-	}
 	bool popBubbles() {
 		//ako je došlo do promjena vrati true
 	}
@@ -102,7 +157,7 @@ public:
 		while (graphChanges)
 		{
 			graphChanges = false;
-			if (removeNodesWithNoEdges()) graphChanges = true;
+			//if (removeNodesWithNoEdges()) graphChanges = true;
 			if (trim()) graphChanges = true;
 			if (popBubbles()) graphChanges = true;
 		}
