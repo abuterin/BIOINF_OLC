cee1025820a898c17cfcd485ed5fc23b9b9a7da9
Ante Buterin <ante.buterin@fer.hr>
2017-01-16 18:53:09 +0100

    Minor change

--- BioInf/BioInf/Walk.cpp
+++ BioInf/BioInf/Walk.cpp
@@ -1,8 +1,15 @@
 #include "Walk.hpp"
 
-vector<Walk*> Walk::extend(bool direction) {
+void Walk::addNode(Vertex * newNode) {
+	_nodes.push_back(newNode); 
+	_lastNode = newNode;
+}
+
+vector<Walk*> Walk::extend(bool direction, Graph* graph) {
 	vector<Edge*> _edges;
 	vector<Walk*> walks;
+	map<unsigned int, Vertex*> vertices = graph->vertices;
+
 	if (direction) {		//if true, egde uses reads start
 		_edges = _lastNode->edges_b;
 	}
@@ -14,16 +21,139 @@ vector<Walk*> Walk::extend(bool direction) {
 		return walks;	//empty vector
 
 	if (_edges.size() == 1) {	//there is no forking
-		_nodes.push_back(_edges[0]->); //ovdje sam stao:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::....
+		if (_edges[0]->overlap->aID() == this->_lastNode->readID) {
+			if (_edges[0]->isInWalk()) {
+				return walks;		//already in walk
+			}
+			_nodes.push_back(vertices[_edges[0]->overlap->bID()]);	//add vertex from the opposite end of the edge
+		}
+		else {
+			if (_edges[0]->isInWalk()) {
+				return walks;		//already in walk
+			}
+			_nodes.push_back(vertices[_edges[0]->overlap->aID()]);	//add vertex from the opposite end of the edge
+		}
+
+		_pathEdges.push_back(_edges[0]);
+		_edges[0]->setInWalk(true);
+		_edges[0]->pair()->setInWalk(true);//mark edge and its pairId edge as part of the walk
+		_lastNode = _nodes.back();
+		walks.push_back(this);
 	}
 	else {
 		for (size_t i = 0; i < _edges.size(); i++) {
 			Walk* newWalk;		//new forked walk
-			if (i == _edges.size() - 1) {	//save last forking in current (this) walk
-				if (_edges[i]->overlap->aID == this->_lastNode->readID) {
+			Edge* edge = _edges[i]; //current edge
 
+			if (i == _edges.size() - 1) {	//save last forking in current (this) walk
+				if (edge->overlap->aID() == this->_lastNode->readID) {
+					if (edge->isInWalk()) {
+						continue;		//already in walk
+					}
+
+					_nodes.push_back(vertices[edge->overlap->bID()]);	//add vertex from the opposite end of the edge
+				}
+				else {
+					if (edge->isInWalk()) {
+						continue;		//already in walk
+					}
+
+					_nodes.push_back(vertices[edge->overlap->aID()]);	//add vertex from the opposite end of the edge
+				}
+
+				_lastNode = _nodes.back();
+				walks.push_back(this);
+			}
+
+			else {
+
+				if (edge->overlap->aID() == this->_lastNode->readID) {
+					if (edge->isInWalk()) {
+						continue;		//already in walk
+					}
+					newWalk = new Walk(this);	//new forked walk
+					newWalk->addNode(vertices[edge->overlap->bID()]);	//add vertex from the opposite end of the edge
+				}
+				else {
+					if (edge->isInWalk()) {
+						continue;		//already in walk
+					}
+
+					newWalk = new Walk(this);	//new forked walk
+					newWalk->addNode(vertices[edge->overlap->aID()]);	//add vertex from the opposite end of the edge
+				}
+				
+				walks.push_back(newWalk);
+			}
+			_pathEdges.push_back(edge);
+			edge->setInWalk(true);
+			edge->pair()->setInWalk(true);//mark edge and its pairId edge as part of the walk
+		}
+	}
+
+	return walks;
+}
+
+void Walk::rewindTo(unsigned int junctionID) {
+	size_t i;
+
+	for (i = _nodes.size() - 1; i > 0; i--) {
+		if (_nodes[i]->readID == junctionID) {
+			break;
+		}
+		else {	//remove edges from walk
+			for (size_t j = _pathEdges.size() - 1; j >= 0; j--) {
+				Edge* edge = _pathEdges[j];
+				if (edge->overlap->aID() == _nodes[i]->readID || edge->overlap->bID() == _nodes[i]->readID) {//if the edge contains current node
+					edge->setInWalk(false);
+					edge->pair()->setInWalk(false);
+					_pathEdges.erase(_pathEdges.begin() + j);		//delete this member from the list
+					break;
 				}
 			}
 		}
 	}
+
+	_lastNode = _nodes[i];
+	_nodes.resize(i + 1);
+	return;
+}
+
+double Walk::coverage(bool direction) {
+	if (_coverage == 0) {
+		/*vector<Edge*> _edges;
+
+		if (direction) {		//if true, egde uses reads start
+			_edges = _lastNode->edges_b;
+		}
+		else {					//otherwise it uses reds end
+			_edges = _lastNode->edges_e;
+		}*/
+		
+
+		for (size_t i = 0; i < _nodes.size(); i++) {
+			_coverage += _nodes[i]->readString.length() * 0.5; //approximate
+		}
+	}
+	
+	return _coverage;
+}
+
+void Walk::extractSequence(string &dest) {
+	if (_pathEdges.empty()) {
+		dest = _nodes[0]->readString;
+		return;
+	}
+
+	auto getType = [](Edge* edge, unsigned int id) -> int {
+		if (edge->overlap->aID() == id) return 0;
+		if (!edge->overlap->isInnie()) return 0;
+		return 1;
+	};
+
+	int startType = getType(_pathEdges.front(), _nodes.front()->readID);
+
+	bool appendToPrefix = (!_pathEdges.front()->overlap->isUsingSuffix(_nodes.front()->readID)) ^ startType;
+
+
 }
--- BioInf/BioInf/Walk.hpp
+++ BioInf/BioInf/Walk.hpp
@@ -11,21 +11,30 @@ class Vertex;
 class Walk {
 private:
 	vector<Vertex*> _nodes; //nodes in a path/walk
+	vector<Edge*> _pathEdges;
 	Vertex* _lastNode;		//for checking wheter sink has been found
+	double _coverage;
 public:
-	Walk(Vertex* startNode) : _lastNode{ startNode } {
+	Walk(Vertex* startNode) : _lastNode{ startNode }, _coverage{ 0 } {
 		_nodes.push_back(startNode);
 	}
-	Walk(Walk* walk) {	//Walk created from forking parental Walk
+	Walk(Walk* walk) : _coverage{ 0 } {	//Walk created from forking parental Walk
 		_nodes = walk->nodes();
 		_lastNode = walk->lastNode();
+		_pathEdges = walk->pathEdges();
 	}
 
 	Vertex* lastNode() { return _lastNode; }
 	vector<Vertex*> nodes() { return _nodes; }
+	vector<Edge*> pathEdges() { return _pathEdges; }
 
-	void addNode(Vertex* newNode) { _nodes.push_back(newNode); _lastNode = newNode; }
+	void addNode(Vertex* newNode);
 
-	vector<Walk*> extend(bool direction); //true if extending has occured, false otherwise
+	vector<Walk*> extend(bool direction, Graph*); //true if extending has occured, false otherwise
 
+	void rewindTo(unsigned int);
+
+	double coverage(bool);
+
+	void extractSequence(string&);
 };
